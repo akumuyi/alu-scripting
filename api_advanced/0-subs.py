1e7626c4704777f5a1756a78ee160029ff655cc8
akumuyi <a.kumuyi@alustudent.com>
2023-11-12 12:45:04 +0000

    0. How many subs?

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,33 +4,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Gets the number of subscribers to the given subreddit.
-
-    Args:
-        subreddit (str): The name of the subreddit to get
-        the number of subscribers for.
-
-    Returns:
-        int: The number of subscribers to the given subreddit.
-    """
-
+    """Gets the number of subscribers to the given subreddit."""
     # Define the Reddit API URL for the given subreddit
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-
     # Set a custom User-Agent to avoid issues
     headers = {
         'User-Agent': 'MyRedditBot/1.0 (by YourUsername)'
     }
-
     try:
         # Send a GET request to the Reddit API
         response = requests.get(url, headers=headers)
-
         # Check if the response is successful
         if response.status_code == 200:
             # Parse the JSON response
             data = response.json()
-
             # Extract and return the number of subscribers
             subscribers = data['data']['subscribers']
             return subscribers
@@ -41,14 +28,3 @@ def number_of_subscribers(subreddit):
         # Request error
         print(f"Error: {e}")
         return 0
-
-
-if __name__ == "__main__":
-    # Get the subreddit name from the command line argument
-    subreddit = sys.argv[1]
-
-    # Get the number of subscribers to the subreddit
-    subscribers = number_of_subscribers(subreddit)
-
-    # Print the number of subscribers to the console
-    print(f"The subreddit {subreddit} has {subscribers} subscribers.")
